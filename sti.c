100064f9d67121710207f9841222d11bdce8f078
Dmytro Dovzhyk <DDovzhyk@luxoft.com>
2017-09-19 19:16:08 +0300

    Перевірка codage, функція витягнення аргументу T_REG

--- sti.c
+++ sti.c
@@ -27,7 +27,7 @@ t_op    op_tab[17] =
 				{0, 0, {0}, 0, 0, 0, 0}
 		};
 
-int		take_reg(unsigned char *map, unsigned int *arg, t_proc *proc)
+int		take_arg_reg(unsigned char *map, unsigned int *arg, t_proc *proc)
 {
 	unsigned char index;
 
@@ -38,81 +38,77 @@ int		take_reg(unsigned char *map, unsigned int *arg, t_proc *proc)
 	return (1);
 }
 
-int		take_ind()
+int		take_arg_dir(unsigned char *map, unsigned int *arg, t_proc *proc)
 {
 
 }
 
-int		take_dir()
+int		take_arg_ind(unsigned char *map, unsigned int *arg, t_proc *proc)
 {
 
 }
 
-void	encode_codage(unsigned char *map, unsigned char codage, t_proc *proc)
+int		take_argument(unsigned char *map, const unsigned char arg_code, unsigned int *arg, t_proc *proc)
 {
-
+	if (arg_code == REG_CODE)
+		return (take_arg_reg(map, arg, proc));
+	else if (arg_code == DIR_CODE)
+		return (take_arg_dir(map, arg, proc));
+	else if (arg_code == IND_CODE)
+		return (take_arg_ind(map, arg, proc));
+	return (0);
 }
 
-/*
- * 	b = 2bites
- *  if (b == (op_tab[command_index].arg[0] & 1))
- *	001
- *	010
- *	100
- *
- *	01
- *	10
- *	11
- */
+int		check_codage(unsigned char *map, unsigned char codage, t_proc *proc,
+						unsigned short opcode)
+{
+    unsigned char tmp;
+	int i;
+	int j;
+
+	i = 8;
+	j = op_tab[opcode].count_arg;
+//	01 10 10 00
+	while (--j >= 0)
+	{
+		i -= 2;
+		//кожен раз зсовуємо, щоб виділити розряди для порівняння:
+		//00 00 00 01, потім 00 00 00 10, 00 00 00 10
+		tmp = (codage >> i) & 0x3; // & 00000011
+//		виділяємо розряд в аргументі який відповідає за тип аргументу
+// 		(T_REG, T_DIR, T_IND), буде 0 або 1, тобто 00000101-> 00000100
+//		(для кожної команди різне) і прирівнюємо його до числа типу (001, 010, 100)
+//		якшо рівні, то ок, в команді в j-му аргументі є такий тип, який закодований
+//		в бінарному коді, якщо ні, то помилка.
+		if (tmp == REG_CODE && ((op_tab[opcode].arg[j] & T_REG) == T_REG))
+			;
+		else if (tmp == DIR_CODE && ((op_tab[opcode].arg[j] & T_REG) == T_REG))
+			;
+		else if (tmp == IND_CODE && ((op_tab[opcode].arg[j] & T_REG) == T_REG))
+			;
+		else
+			return (0);
+	}
+	//перевіряємо на нуль останні розряди (4 - count_arg)
+	j = 4 - op_tab[opcode].count_arg;
+	while (j > 0)
+	{
+		i -= 2;
+		tmp = (codage >> i) & 0x3;
+		if (tmp != 0)
+			return (0);
+	}
+	return (1);
+}
 
 void	sti(unsigned char *map, t_proc *proc)
 {
 	unsigned int arg[3];
-	unsigned int i;
-	unsigned char tmp;
 
-	i = 0;
-	//01 10 10 00
-	if ((map[0] << 6) != 0)
-		return ;
-	tmp = map[0] >> 6;
-	if (tmp != T_REG || !take_reg(map, arg, proc))
-		return ;
-	tmp = map[0] << 2;
-	tmp >>= 6;
-	if (tmp == T_REG)
-	{
-		if (!take_reg(map, arg, proc))
-			return ;
-	}
-	else if (tmp == T_DIR)
-	{
-
-	}
-	else if (tmp == T_IND)
-	{
-	}
-	else
-		return ;
-
-	tmp = map[0] << 4;
-	tmp >>= 6;
-	if (tmp == T_DIR)
-	{
-
-	}
-	else if (tmp == T_REG)
-	{
-
-	}
-	else
-		return ;
-
-	//encode_codage(map, , proc);
+//	TODO доробити основу
 }
 
-//11
-//10
-//01
-//
-//01 10 10 00
+//TODO написати функцію яка буде:
+//1) Перевіряти на валідність opcode
+//2) Викликати check_codage
+//3) Викликати саму команду в залежності від opcode
