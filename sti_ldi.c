f6d5ccbae6053d27618c5b3890615a13fc30e0f7
ddovzhik <dimon.dovjik@gmail.com>
2017-10-07 20:24:43 +0300

    Виправив sti + ldi

--- sti_ldi.c
+++ sti_ldi.c
@@ -18,45 +18,36 @@ int		for_instruct(unsigned char *map, t_proc *proc, unsigned short opcode,
 	return (1);
 }
 
-int		get_all_args(unsigned char *map, t_proc *proc,
-						unsigned char *opcode_codage, unsigned int *arg)
-{
-	unsigned char arg_code_size_flag[2];
-	int i;
-	int j;
-
-	i = 8;
-	j = 0;
-	while (j < op_tab[opcode_codage[0] - 1].count_arg)
-	{
-		i -= 2;
-		arg_code_size_flag[0] = (unsigned char)((opcode_codage[1] >> i) & 0x3);
-		arg_code_size_flag[1] = op_tab[opcode_codage[0] - 1].dir_size;
-		if (!take_argument(map, arg_code_size_flag, arg + j, proc))
-			return (0);
-		++j;
-	}
-	return (1);
-}
-
 int		sti(unsigned char *map, t_proc *proc, unsigned short opcode)
 {
 	unsigned int arg[3];
 	unsigned char codage;
-	unsigned char opcode_codage[2];
+	unsigned char arg_code_size_flag[2];
+	int i;
+	int j;
 
 	if (for_instruct(map, proc, opcode, &codage) == 0)
 		//TODO реакція на провалену перевірку codage
 		return (0);
-
-	opcode_codage[0] = (char)opcode;
-	opcode_codage[1] = codage;
-	if (get_all_args(map, proc, opcode_codage, arg) == 0)
-		//TODO реакція на провалену перевірку take_arg
-		return (0);
-
-	map += (arg[1] + arg[2]) % IDX_MOD;
-	*((unsigned int *)map) = proc->regs[arg[0]];
+	i = 8;
+	j = 0;
+	while (j < op_tab[opcode - 1].count_arg)
+	{
+		i -= 2;
+		arg_code_size_flag[0] = (unsigned char)((codage >> i) & 0x3);
+		arg_code_size_flag[1] = op_tab[opcode - 1].dir_size;
+		if (!take_argument(map, arg_code_size_flag, arg + j, proc))
+			return (0);
+		if (codage == T_REG)
+			arg[j] = proc->regs[arg[j]];
+		++j;
+	}
+	proc->pc += (arg[1] + arg[2]) % IDX_MOD;
+	map[proc->pc++ % MEM_SIZE] = (unsigned char)((arg[0] >> 6) & 0x3);
+	// перевірити на відповідність big endian i little endian
+	map[proc->pc++ % MEM_SIZE] = (unsigned char)((arg[0] >> 4) & 0x3);
+	map[proc->pc++ % MEM_SIZE] = (unsigned char)((arg[0] >> 2) & 0x3);
+	map[proc->pc++ % MEM_SIZE] = (unsigned char)(arg[0] & 0x3);
 	proc->inst_cycle = 0;
 	return (1);
 }
@@ -65,20 +56,32 @@ int		ldi(unsigned char *map, t_proc *proc, unsigned short opcode)
 {
 	unsigned int arg[3];
 	unsigned char codage;
-	unsigned char opcode_codage[2];
+	unsigned char arg_code_size_flag[2];
+	int i;
+	int j;
 
 	if (for_instruct(map, proc, opcode, &codage) == 0)
 		//TODO реакція на провалену перевірку codage
 		return (0);
-
-	opcode_codage[0] = (char)opcode;
-	opcode_codage[1] = codage;
-	if (get_all_args(map, proc, opcode_codage, arg) == 0)
-		//TODO реакція на провалену перевірку take_arg
-		return (0);
-
-	map += (arg[0] + arg[1]) % IDX_MOD;
-	proc->regs[arg[0]] = *((unsigned int *)map);
+	i = 8;
+	j = 0;
+	while (j < op_tab[opcode - 1].count_arg)
+	{
+		i -= 2;
+		arg_code_size_flag[0] = (unsigned char)((codage >> i) & 0x3);
+		arg_code_size_flag[1] = op_tab[opcode - 1].dir_size;
+		if (!take_argument(map, arg_code_size_flag, arg + j, proc))
+			return (0);
+		if (j != 2)
+			arg[j] = proc->regs[arg[j]];
+		++j;
+	}
+	proc->pc = (proc->pc + (arg[0] + arg[1]) % IDX_MOD) % MEM_SIZE;
+	i = map[proc->pc++];
+	i = (i << 8) | (unsigned int)map[proc->pc++ % MEM_SIZE];
+	i = (i << 8) | (unsigned int)map[proc->pc++ % MEM_SIZE];
+	i = (i << 8) | (unsigned int)map[proc->pc++ % MEM_SIZE];
+	proc->regs[arg[0]] = (unsigned int)i;
 	proc->inst_cycle = 0;
 	return (1);
 }
