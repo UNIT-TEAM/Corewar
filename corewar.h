c229afbfe78f2ba47c7e623d5546c962c29773b2
Yaroslav Savenko <ysavenko@e1r9p16.unit.ua>
2017-10-23 21:26:32 +0300

    .h norm

--- corewar.h
+++ corewar.h
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corewar.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysavenko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/23 20:49:02 by ysavenko          #+#    #+#             */
+/*   Updated: 2017/10/23 21:26:10 by ysavenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef COREWAR_H
-#define COREWAR_H
+# define COREWAR_H
 
 # include "./libft/libft.h"
 # include <fcntl.h>
 # include <stdio.h>
 # include "op.h"
-#include "ncurses.h"
+# include "ncurses.h"
+
 /*
 **	p		- players in project;
 ** 	np		- number of players;
@@ -17,36 +30,35 @@
 
 # define COMMENT_POS	(PROG_NAME_LENGTH + 13 - ((PROG_NAME_LENGTH + 1) % 4))
 # define SIZE_POS		(PROG_NAME_LENGTH + 9 - ((PROG_NAME_LENGTH + 1) % 4))
-# define CODE_POS		(PROG_NAME_LENGTH + 10 + COMMENT_LENGTH + ((4 - \
-						((PROG_NAME_LENGTH + 1) % 4)) * 2))
-// !!!!!!!!!!!!!!!!!!!!!!
+# define POD			((4 - ((PROG_NAME_LENGTH + 1) % 4)) * 2))
+# define CODE_POS		(PROG_NAME_LENGTH + 10 + COMMENT_LENGTH + POD
 
 typedef struct		s_op
 {
-    char			*name;
-    unsigned short	count_arg;
-    t_arg_type		arg[3];
-    unsigned short	opcode;
-    unsigned short	cycles;
-    unsigned char 	is_codage;
-    unsigned char	dir_size;
+	char			*name;
+	unsigned short	count_arg;
+	t_arg_type		arg[3];
+	unsigned short	opcode;
+	unsigned short	cycles;
+	unsigned char	is_codage;
+	unsigned char	dir_size;
 }					t_op;
 
-typedef struct 		s_color
+typedef struct		s_color
 {
-	char 			champ;
-	int 			cycle_n;
-    int             carretka;
-    int             live;
+	char			champ;
+	int				cycle_n;
+	int				carretka;
+	int				live;
 }					t_color;
 
-typedef struct 		s_proc
+typedef struct		s_proc
 {
 	unsigned int	pc;
 	unsigned int	regs[REG_NUMBER];
 	unsigned short	carry;
 	unsigned short	is_live;
-    unsigned int    inst_cycle;
+	unsigned int	inst_cycle;
 	unsigned int	opcode;
 	unsigned int	id;
 
@@ -56,9 +68,9 @@ typedef struct 		s_proc
 typedef struct		s_chmp
 {
 	header_t		head;
-	int 			live;
+	int				live;
 	unsigned int	cycle_live;
-	unsigned int 	num;
+	unsigned int	num;
 	unsigned char	*instructions;
 	unsigned int	flag_num;
 	struct s_chmp	*next;
@@ -68,13 +80,13 @@ typedef	struct		s_bs
 {
 	t_chmp			*list_champs;
 	t_proc			*list_proc;
-	unsigned char 	map[MEM_SIZE];
+	unsigned char	map[MEM_SIZE];
 	t_color			color_map[MEM_SIZE];
-	int 			np;
+	int				np;
 	int				num_live;
 	unsigned int	winner;
 	unsigned short	is_num_flag;
-	unsigned int 	dump;
+	unsigned int	dump;
 	unsigned short	is_dump;
 	unsigned int	cycle_print;
 	unsigned short	is_print;
@@ -85,80 +97,74 @@ typedef	struct		s_bs
 
 typedef struct		s_ncur
 {
-    WINDOW          *window;
-    int             flag;
-    int             n_cyc;
-	long 			cyc_to_die;
-	int 			a;
-	int 			b;
-	int 			ch;
-	unsigned int cy;
+	WINDOW			*window;
+	int				flag;
+	int				n_cyc;
+	long			cyc_to_die;
+	int				a;
+	int				b;
+	int				ch;
+	unsigned int	cy;
 
-}                   t_ncurs;
+}					t_ncurs;
 
-extern  t_op		op_tab[17];
-extern unsigned int g_count;
+extern t_op			op_tab[17];
+extern unsigned int	g_count;
 
-void 	ft_error(int i, char *str);
+void				ft_error(int i, char *str);
 
-void 	fill_magic(unsigned int *magic_size, unsigned char *buf, char *str,\
-				int flag);
-void 	ft_magic_size(char *av, header_t *p);
-
-void 	ft_name_comment(char *av, header_t *p, int flag);
-void	ft_instraction(char *av, t_bs *bs);
-
-void	add_new_champ(t_chmp **first, unsigned int num_player, t_proc **proc);
-
-void 	ft_fill_map(t_bs *bs);
-void	print_map(unsigned char *map);
-
-int		check_op_h();
-
-unsigned int	*take_argument(unsigned char *map, unsigned char codage,
-							   t_proc *proc, unsigned short op_index,
-							   short flag_long);
-unsigned int	take_value_from_addres(unsigned char *map, t_proc *proc,
-									   short address, short flag_long);
-
-int		check_codage(unsigned char codage, t_proc *proc, unsigned short index);
-
-void	shift_pc(unsigned char codage, t_proc *proc, unsigned short op_index);
-
-
-void	ft_live(t_bs *bs, t_proc *proc, unsigned short op_index,
-				t_chmp *champs);
-void	ft_ld_lld_ldi_lldi(unsigned char *map, t_proc *proc,
-						   unsigned short op_index);
-void	ft_st_sti(t_bs *bs, t_proc *proc, unsigned short op_index);
-void	ft_add_sub_and_or_xor(unsigned char *map, t_proc *proc,
-							  unsigned short op_index);
-void	ft_zjump(unsigned char *map, t_proc *proc, unsigned short index);
-void	ft_fork_lfork(unsigned char *map, t_proc **procs, t_proc *tmp,
-					  unsigned short op_index);
-void ft_aff(unsigned char *map, t_proc *proc, unsigned short op_index,
-			unsigned short is_aff);
-
-
-void	add_color(int a, unsigned int champ, t_bs *bs);
-void	ncurses_init_win(t_ncurs *base);
-int		kb_proc(t_ncurs *base, int ch);
-void	draw_mass(t_bs *bs, int a, int b);
-void    ncurses_init_colors();
-void create_box(WINDOW *p_win, int size);
-void    ncurses_stats(WINDOW *window, t_ncurs *base, t_bs *bs, int a);
-void    ncurses_result(t_ncurs *base, t_bs *bs);
-int			check_is_live(t_proc **procs, unsigned short is_beep, t_proc *tmp);
-void		check_inst_proc(t_bs *bs, t_proc **procs, unsigned char *map,
-                            t_chmp *champs);
-int			check_cycle_to_die(t_bs *bs, long *cycle_to_die,
-                                  unsigned int *max_check,
-                                  unsigned int *cycle_to_die_curr);
-void	set_chmps_with_flag_num(t_bs *bs);
-void	set_chmps_without_flag_num(t_bs *bs, unsigned int i, int k,
-                                   t_proc *tmp_proc);
-void	g_c_prep(t_ncurs *ncurs, t_bs *bs);
-void		who_win(t_chmp *champ, unsigned int *winner);
-
+void				fill_magic(unsigned int *magic_size, unsigned char *buf,
+					char *str, int flag);
+void				ft_magic_size(char *av, header_t *p);
+void				ft_name_comment(char *av, header_t *p, int flag);
+void				ft_instraction(char *av, t_bs *bs);
+void				add_new_champ(t_chmp **first, unsigned int num_player,
+					t_proc **proc);
+void				ft_fill_map(t_bs *bs);
+void				print_map(unsigned char *map);
+int					check_op_h();
+unsigned int		*take_argument(unsigned char *map, unsigned char codage,
+					t_proc *proc, unsigned short op_index,
+					short flag_long);
+unsigned int		take_value_from_addres(unsigned char *map, t_proc *proc,
+					short address, short flag_long);
+int					check_codage(unsigned char codage, t_proc *proc,
+					unsigned short index);
+void				shift_pc(unsigned char codage, t_proc *proc,
+					unsigned short op_index);
+void				ft_live(t_bs *bs, t_proc *proc, unsigned short op_index,
+					t_chmp *champs);
+void				ft_ld_lld_ldi_lldi(unsigned char *map, t_proc *proc,
+					unsigned short op_index);
+void				ft_st_sti(t_bs *bs, t_proc *proc, unsigned short op_index);
+void				ft_add_sub_and_or_xor(unsigned char *map, t_proc *proc,
+					unsigned short op_index);
+void				ft_zjump(unsigned char *map, t_proc *proc,
+					unsigned short index);
+void				ft_fork_lfork(unsigned char *map, t_proc **procs,
+					t_proc *tmp, unsigned short op_index);
+void				ft_aff(unsigned char *map, t_proc *proc,
+					unsigned short op_index, unsigned short is_aff);
+void				add_color(int a, unsigned int champ, t_bs *bs);
+void				ncurses_init_win(t_ncurs *base);
+int					kb_proc(t_ncurs *base, int ch);
+void				draw_mass(t_bs *bs, int a, int b);
+void				ncurses_init_colors(void);
+void				create_box(WINDOW *p_win, int size);
+void				ncurses_stats(WINDOW *window, t_ncurs *base, t_bs *bs,
+					int a);
+void				ncurses_result(t_ncurs *base, t_bs *bs);
+int					check_is_live(t_proc **procs, unsigned short is_beep,
+					t_proc *tmp);
+void				check_inst_proc(t_bs *bs, t_proc **procs,
+					unsigned char *map, t_chmp *champs);
+int					check_cycle_to_die(t_bs *bs, long *cycle_to_die,
+					unsigned int *max_check,
+					unsigned int *cycle_to_die_curr);
+void				set_chmps_with_flag_num(t_bs *bs);
+void				set_chmps_without_flag_num(t_bs *bs, unsigned int i, int k,
+					t_proc *tmp_proc);
+void				g_c_prep(t_ncurs *ncurs, t_bs *bs);
+void				who_win(t_chmp *champ, unsigned int *winner);
 
 #endif
